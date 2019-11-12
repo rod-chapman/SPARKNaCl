70b955f31261981ae984c619384d360290520f0f
Rod Chapman <rod@proteancode.com>
2019-11-12 22:58:35 +0000

    Add configuration pragms Restrictions, and add warning that Random is not secure.

--- src/random.adb
+++ src/random.adb
@@ -2,6 +2,9 @@ with Ada.Numerics.Discrete_Random;
 package body Random
   with SPARK_Mode => Off
 is
+   pragma Compile_Time_Warning
+     (True, "This PRNG is not cryptographically secure.");
+
    package PRNG is new Ada.Numerics.Discrete_Random (Interfaces.Unsigned_8);
 
    Gen : PRNG.Generator;
