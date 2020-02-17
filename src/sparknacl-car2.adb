5e0211a7f57fabba393278ad5a897c2caa46198e
Rod Chapman <rod@proteancode.com>
2020-02-17 14:53:22 +0000

    Full proof of new implementation of Product_To_Seminormal using constant Ghost lookup tables to yield upper bounds on limbs and carry values.

--- src/sparknacl-car2.adb
+++ src/sparknacl-car2.adb
@@ -3,29 +3,16 @@
 package body SPARKNaCl.Car2
   with SPARK_Mode => On
 is
-   --  "UB" = "Upper Bound"
-
-   --  Upperbound on _input_ limbs, as per the definition
-   --  of Product_GF
-   function Limb_UB (I : in Index_16) return I64 is
-      ((MGFLC - 37 * GF_Any_Limb (I)) * MGFLP);
-
-   --  LnC = "Limb and Carry"
-   --  Note: recursive definition!
-   function LnC_UB (I : in Index_16) return I64 is
-      (if I = 0 then (Limb_UB (0))
-                else (Limb_UB (I) + (LnC_UB (I - 1) / 65536)));
-
-   function Carry_UB (I : in Index_16) return I64 is
-      (LnC_UB (I) / 65536);
-
-
    --  Simple translation of Donenfeld's original C code from
    --  WireGuard.  Here for reference as an oracle for testing
    procedure Carry (R : in out GF)
      with SPARK_Mode => Off
    is
    begin
+--      for I in Index_16 loop
+--         Debug.DHH (I'Img & " => ", LnC_UB (I));
+--      end loop;
+
       --  for (i = 0; i < 16; ++i) {
       --    o[(i + 1) % 16] += (i == 15 ? 38 : 1) * (o[i] >> 16);
       --    o[i] &= 0xffff;
@@ -52,38 +39,24 @@ is
           (for all I in Index_16 =>
              Temp_GF (I) >= 0 and Temp_GF (I) <= MGFLC * MGFLP);
 
-      subtype Carry_Adjustment is I64 range 0 .. (MGFLC * MGFLP) / 65536;
-      Carry : Carry_Adjustment;
+      subtype Carry_T is I64 range 0 .. I64'Last;
+      Carry : Carry_T;
       R     : Temp_GF;
    begin
       R := X;
+
       for I in Index_16 range 0 .. 14 loop
-         Carry := ASR_16 (R (I));
-
-         pragma Assert
-           (X (I + 1) <= (MGFLC - 37 * GF_Any_Limb (I + 1)) * MGFLP);
-         pragma Assert
-           (X (I) <= (MGFLC - 37 * GF_Any_Limb (I)) * MGFLP);
-         pragma Assert (R (I + 1) = X (I + 1));
-
+         Carry := R (I) / 65536;
          R (I + 1) := R (I + 1) + Carry;
          R (I) := R (I) mod 65536;
 
          pragma Loop_Invariant
            (for all K in Index_16 range 0 .. I => (R (K) in GF_Normal_Limb));
          pragma Loop_Invariant (R (I + 1) >= 0);
-
---  Previously, we had...
---         pragma Loop_Invariant
---           (R (I + 1) <= (MGFLC - 37 * GF_Any_Limb (I)) * MGFLP);
-
---  But now we can be much more precise using the functions above...
          pragma Loop_Invariant (Carry     <= Carry_UB (I));
          pragma Loop_Invariant (R (I + 1) <= LnC_UB (I + 1));
-
          pragma Loop_Invariant
            (for all K in Index_16 range I + 2 .. 15 => R (K) = X (K));
-
       end loop;
 
       --  Substitute I = 14 into the above loop invariant
@@ -92,23 +65,13 @@ is
         (for all K in Index_16 range 0 .. 14 => (R (K) in GF_Normal_Limb));
       pragma Assert (R (15) <= LnC_UB (15));
 
---  which is much stronger than what we had before:
---  pragma Assert (R (15) in 0 .. 53 * MGFLP);
---  SO... we should be able to place a much stronger bound on R (0)
-
-      Carry := ASR_16 (R (15));
-
-      pragma Assert (Carry <= Carry_UB (15));
-
+      Carry := R (15) / 65536;
       R (0) := R (0) + 38 * Carry;
-
-      --  Declaration of Seminormal_GF_LSL needs to be adjusted...
-      --  Seminormal_GF_LSL'Last could be = 65535 + 38 * CarryUB (15) now...
-      pragma Assert (R (0) in Seminormal_GF_LSL);
-
       R (15) := R (15) mod 65536;
 
-      return Seminormal_GF (R);
+      --  The upper-bound on R (15) is sufficient to get a tight upper-bound
+      --  on R (0) now, so...
+      return New_Seminormal_GF (R);
    end Product_To_Seminormal;
 
 
--- src/sparknacl-car2.ads
+++ src/sparknacl-car2.ads
@@ -14,10 +14,78 @@ is
    --  WireGuard.  Here for reference as an oracle for testing
    procedure Carry (R : in out GF);
 
+
+
+   type Limb_LUT is array (Index_16) of I64 with Ghost;
+
+   --  "UB" = "Upper Bound"
+
+   --  Upperbound on _input_ limbs, as per the definition
+   --  of Product_GF
+   --  function Limb_UB (I : in Index_16) return I64 is
+   --     ((MGFLC - 37 * GF_Any_Limb (I)) * MGFLP);
+
+   --  LnC = "Limb and Carry"
+   --  Note: recursive definition!
+   --  function LnC_UB (I : in Index_16) return I64 is
+   --     (if I = 0 then (Limb_UB (0))
+   --               else (Limb_UB (I) + (LnC_UB (I - 1) / 65536)));
+   LnC_UB : constant Limb_LUT :=
+     (0 => 16#23AFB8A023B#,
+      1 => 16#215FE0EFDA0#,
+      2 => 16#1F0FE33FFFF#,
+      3 => 16#1CBFE58FFFF#,
+      4 => 16#1A6FE7DFFFF#,
+      5 => 16#181FEA2FFFF#,
+      6 => 16#15CFEC7FFFF#,
+      7 => 16#137FEECFFFF#,
+      8 => 16#112FF11FFFF#,
+      9 => 16#EDFF36FFFF#,
+      10 => 16#C8FF5BFFFF#,
+      11 => 16#A3FF80FFFF#,
+      12 => 16#7EFFA5FFFF#,
+      13 => 16#59FFCAFFFF#,
+      14 => 16#34FFEFFFFF#,
+      15 => 16#100014FFFF#) with Ghost;
+
+   --   function Carry_UB (I : in Index_16) return I64 is
+   --      (LnC_UB (I) / 65536);
+   Carry_UB : constant Limb_LUT :=
+     (0 => 16#23AFB8A#,
+      1 => 16#215FE0E#,
+      2 => 16#1F0FE33#,
+      3 => 16#1CBFE58#,
+      4 => 16#1A6FE7D#,
+      5 => 16#181FEA2#,
+      6 => 16#15CFEC7#,
+      7 => 16#137FEEC#,
+      8 => 16#112FF11#,
+      9 => 16#EDFF36#,
+      10 => 16#C8FF5B#,
+      11 => 16#A3FF80#,
+      12 => 16#7EFFA5#,
+      13 => 16#59FFCA#,
+      14 => 16#34FFEF#,
+      15 => 16#100014#) with Ghost;
+
+   --  Distinct named number here so the table above can be "Ghost".
+   Carry_UB_15 : constant := 16#100014#;
+   subtype New_Seminormal_GF_LSL is I64
+     range 0 .. 65535 + 38 * Carry_UB_15;
+
+   --  Limbs 1 though 15 are in 0 .. 65535, but the
+   --  Least Significant Limb 0 is in GF_Seminormal_Product_LSL
+   subtype New_Seminormal_GF is GF
+     with Dynamic_Predicate =>
+       (New_Seminormal_GF (0) in New_Seminormal_GF_LSL and
+         (for all I in Index_16 range 1 .. 15 =>
+           New_Seminormal_GF (I) in GF_Normal_Limb));
+
+
    --  Supporting "*"
    function Product_To_Seminormal
     (X : in Product_GF)
-       return Seminormal_GF
+       return New_Seminormal_GF
      with Global => null;
 
 end SPARKNaCl.Car2;
