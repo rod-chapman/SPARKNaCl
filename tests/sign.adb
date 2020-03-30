094f7562636709acf3624ae457616421b33c351c
Rod Chapman <rod@proteancode.com>
2020-03-30 11:12:12 +0100

    Only report significant bytes of result in Sign.

--- tests/sign.adb
+++ tests/sign.adb
@@ -2,6 +2,7 @@ with SPARKNaCl;       use SPARKNaCl;
 with SPARKNaCl.Debug; use SPARKNaCl.Debug;
 with SPARKNaCl.Sign;  use SPARKNaCl.Sign;
 with Ada.Text_IO;     use Ada.Text_IO;
+with Interfaces;      use Interfaces;
 procedure Sign
 is
    PK : Signing_PK;
@@ -11,16 +12,38 @@ is
 
    SM : Byte_Seq (0 .. 319) := (others => 0);
    M2 : Byte_Seq (0 .. 319) := (others => 0);
+   M3 : Byte_Seq (0 .. 255);
 
    ML : I32;
    S  : Boolean;
+--   I  : I64 := 1;
 begin
-   Keypair (PK, SK);
+--   loop
+--      Put_Line ("Iteration " & I'Img);
+      Keypair (PK, SK);
 
-   Sign (SM, M, SK);
+      begin
+         Sign (SM, M, SK);
+      exception
+         when Constraint_Error =>
+            Debug.DH ("In Sign, SK was ", Serialize (SK));
+            raise;
+      end;
 
-   Open (M2, S, ML, SM, PK);
-   DH ("M2 is ", M2);
-   Put_Line ("Status is " & S'Img);
-   Put_Line ("ML is " & ML'Img);
+      begin
+         Open (M2, S, ML, SM, PK);
+      exception
+         when Constraint_Error =>
+            Debug.DH ("In Open, PK was ", Serialize (PK));
+            Debug.DH ("In Open, SM was ", SM);
+            raise;
+      end;
+
+      M3 := M2 (0 .. 255);
+
+--      I := I + 1;
+      DH ("M3 is ", M3);
+      Put_Line ("Status is " & S'Img);
+      Put_Line ("ML is " & ML'Img);
+--   end loop;
 end Sign;
