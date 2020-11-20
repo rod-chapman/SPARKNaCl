c3036ea8dba2fd298ad6ee53ee8d0aadc6a1f05b
Rod Chapman <rod@proteancode.com>
2020-11-20 09:41:47 +0000

    Use objdump to generate details code size info.

--- perf/tsize.mk
+++ perf/tsize.mk
@@ -14,9 +14,17 @@ sizes.txt: tsize.adb io.adb io.ads tweetnacl_api.ads tweetnacl.c
 	@echo "Results for TweetNaCl" >>sizes.txt
 	riscv32-elf-size -t tweetnacl.o
 	@riscv32-elf-size -t tweetnacl.o >>sizes.txt
+	riscv32-elf-objdump -h $(SIZE_OBJS) | fgrep ".text." >st.txt
+	riscv32-elf-objdump -h $(SIZE_OBJS) | fgrep ".rodata." >sd.txt
+	riscv32-elf-objdump -h tweetnacl.o | fgrep ".text." >tt.txt
+	riscv32-elf-objdump -h tweetnacl.o | fgrep ".rodata." >td.txt
 
 clean:
 	rm -f sizes.txt
+	rm -f st.txt
+	rm -f sd.txt
+	rm -f tt.txt
+	rm -f td.txt
 	rm -f tsize.map
 	rm -f d.ci
 	rm -f devurandom.ci
