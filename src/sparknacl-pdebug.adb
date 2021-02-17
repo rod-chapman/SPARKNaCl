bd3dc0630807a4287802971b90f7e41c5242d985
Rod Chapman <rod@proteancode.com>
2021-02-17 17:03:09 +0000

    Add logging procedures for GF32 and Normal_GF

--- src/sparknacl-pdebug.adb
+++ src/sparknacl-pdebug.adb
@@ -5,7 +5,7 @@ is
 
    package I64IO is new Ada.Text_IO.Integer_IO (Integer_64);
 
-   procedure DH (S : in String; D : in GF32)
+   procedure DH16 (S : in String; D : in Normal_GF)
    is
    begin
       if On then
@@ -17,7 +17,21 @@ is
             New_Line;
          end loop;
       end if;
-   end DH;
+   end DH16;
+
+   procedure DH32 (S : in String; D : in GF32)
+   is
+   begin
+      if On then
+         Put_Line (S);
+         for I in D'Range loop
+            I64IO.Put (I64 (D (I)), Width => 0);
+            Put (' ');
+            I64IO.Put (I64 (D (I)), Width => 0, Base => 16);
+            New_Line;
+         end loop;
+      end if;
+   end DH32;
 
    procedure DH64 (S : in String; D : in GF64)
    is
--- src/sparknacl-pdebug.ads
+++ src/sparknacl-pdebug.ads
@@ -2,7 +2,9 @@ private package SPARKNaCl.PDebug
   with SPARK_Mode => On
 is
 
-   procedure DH  (S : in String; D : in GF32);
+   procedure DH16  (S : in String; D : in Normal_GF);
+
+   procedure DH32  (S : in String; D : in GF32);
 
    procedure DH64  (S : in String; D : in GF64);
 
