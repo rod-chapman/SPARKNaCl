1d37c33397095e49f579e1aef002ee34cc1ed7c2
Rod Chapman <rod@proteancode.com>
2021-03-10 14:51:32 +0000

    Updates for new representation of Normal_GF

--- stress/sparknacl-tests.adb
+++ stress/sparknacl-tests.adb
@@ -5,46 +5,47 @@ with SPARKNaCl.Car;
 with Ada.Text_IO; use Ada.Text_IO;
 package body SPARKNaCl.Tests
 is
-   GF_2 : constant GF32 := (2, others => 0);
+   GF_2 : constant Normal_GF := (2, others => 0);
 
-   P : constant GF32 := (0      => 16#FFED#,
-                         15     => 16#7FFF#,
-                         others => 16#FFFF#);
+   P : constant Normal_GF := (0      => 16#FFED#,
+                              15     => 16#7FFF#,
+                              others => 16#FFFF#);
 
-   P_Minus_1 : constant GF32 := (0      => 16#FFEC#,
-                                 15     => 16#7FFF#,
-                                 others => 16#FFFF#);
+   P_Minus_1 : constant Normal_GF := (0      => 16#FFEC#,
+                                      15     => 16#7FFF#,
+                                      others => 16#FFFF#);
 
-   P_Plus_1 : constant GF32 := (0      => 16#FFEE#,
-                                15     => 16#7FFF#,
-                                others => 16#FFFF#);
+   P_Plus_1 : constant Normal_GF := (0      => 16#FFEE#,
+                                     15     => 16#7FFF#,
+                                     others => 16#FFFF#);
 
    procedure GF_Stress
    is
-      A, B, C : GF32;
+      A, B, C : Normal_GF;
+      C2      : GF32;
       D       : Product_GF;
    begin
       Put_Line ("GF_Stress case 1 - 0 * 0");
       A := (others => 0);
       B := (others => 0);
       C := A * B;
-      PDebug.DH ("Result is", C);
+      PDebug.DH16 ("Result is", C);
 
       Put_Line ("GF_Stress case 2 - FFFF * FFFF");
       A := (others => 16#FFFF#);
       B := (others => 16#FFFF#);
       C := A * B;
-      PDebug.DH ("Result is", C);
+      PDebug.DH16 ("Result is", C);
 
       Put_Line ("GF_Stress case 3 - Seminormal Product GF Max");
       D := (0 => 132_051_011, others => 16#FFFF#);
-      C := Car.Product_To_Seminormal (D);
-      PDebug.DH ("Result is", C);
+      C2 := Car.Product_To_Seminormal (D);
+      PDebug.DH32 ("Result is", C2);
 
       Put_Line ("GF_Stress case 4 - Seminormal Product tricky case");
       D := (0 => 131_071, others => 16#FFFF#);
-      C := Car.Product_To_Seminormal (D);
-      PDebug.DH ("Result is", C);
+      C2 := Car.Product_To_Seminormal (D);
+      PDebug.DH32 ("Result is", C2);
    end GF_Stress;
 
    procedure Car_Stress
@@ -59,11 +60,11 @@ is
       A := (0 .. 14 => 65535, 15 => 65535 + 2**32);
       PDebug.DH64 ("Case 1 - A is", A);
       SN := Car.Product_To_Seminormal (A);
-      PDebug.DH ("Case 1 - SN is", SN);
+      PDebug.DH32 ("Case 1 - SN is", SN);
       NGF := Car.Seminormal_To_Nearlynormal (SN);
-      PDebug.DH ("Case 1 - NGF is", NGF);
+      PDebug.DH32 ("Case 1 - NGF is", NGF);
       R := Car.Nearlynormal_To_Normal (NGF);
-      PDebug.DH ("Case 1 - R is", R);
+      PDebug.DH16 ("Case 1 - R is", R);
 
       --  Case 2 Upper bounds on all limbs of a Product_GF
       C := (0 => MGFLC * MGFLP,
@@ -84,11 +85,11 @@ is
             15 => (MGFLC - 37 * 15) * MGFLP);
       PDebug.DH64 ("Case 2 - C is", C);
       SN := Car.Product_To_Seminormal (C);
-      PDebug.DH ("Case 2 - SN is", SN);
+      PDebug.DH32 ("Case 2 - SN is", SN);
       NGF := Car.Seminormal_To_Nearlynormal (SN);
-      PDebug.DH ("Case 2 - NGF is", NGF);
+      PDebug.DH32 ("Case 2 - NGF is", NGF);
       R := Car.Nearlynormal_To_Normal (NGF);
-      PDebug.DH ("Case 2 - R is", R);
+      PDebug.DH16 ("Case 2 - R is", R);
 
 
       --  Intermediate (pre-normalization) result of
@@ -111,11 +112,11 @@ is
             16#FFFE00010#);
       PDebug.DH64 ("Case 3 - A is", A);
       SN := Car.Product_To_Seminormal (A);
-      PDebug.DH ("Case 3 - SN is", SN);
+      PDebug.DH32 ("Case 3 - SN is", SN);
       NGF := Car.Seminormal_To_Nearlynormal (SN);
-      PDebug.DH ("Case 3 - NGF is", NGF);
+      PDebug.DH32 ("Case 3 - NGF is", NGF);
       R := Car.Nearlynormal_To_Normal (NGF);
-      PDebug.DH ("Case 3 - R is", R);
+      PDebug.DH16 ("Case 3 - R is", R);
 
 
 
@@ -128,28 +129,28 @@ is
       R : Normal_GF;
    begin
       C := (others => 0);
-      PDebug.DH ("Case 1 - C is", C);
+      PDebug.DH32 ("Case 1 - C is", C);
       R := Car.Nearlynormal_To_Normal (C);
-      PDebug.DH ("Case 1 - R is", R);
+      PDebug.DH16 ("Case 1 - R is", R);
 
       C := (others => 16#ffff#);
-      PDebug.DH ("Case 2 - C is", C);
+      PDebug.DH32 ("Case 2 - C is", C);
       R := Car.Nearlynormal_To_Normal (C);
-      PDebug.DH ("Case 2 - R is", R);
+      PDebug.DH16 ("Case 2 - R is", R);
 
       for I in I32 range 65536 .. 65573 loop
          C (0) := I;
-         PDebug.DH ("Case 3," & I'Img & " C is", C);
+         PDebug.DH32 ("Case 3," & I'Img & " C is", C);
          R := Car.Nearlynormal_To_Normal (C);
-         PDebug.DH ("Case 3 - R is", R);
+         PDebug.DH16 ("Case 3 - R is", R);
       end loop;
 
       C := (others => 0);
       for I in I32 range -38 .. -1 loop
          C (0) := I;
-         PDebug.DH ("Case 4," & I'Img & " C is", C);
+         PDebug.DH32 ("Case 4," & I'Img & " C is", C);
          R := Car.Nearlynormal_To_Normal (C);
-         PDebug.DH ("Case 4 - R is", R);
+         PDebug.DH16 ("Case 4 - R is", R);
       end loop;
 
 
@@ -159,49 +160,49 @@ is
 
    procedure Pack_Stress
    is
-      A : GF32;
+      A : Normal_GF;
       R : Bytes_32;
-      Two_P : constant GF32 := P * GF_2;
-      Two_P_Minus_1 : constant GF32 := Two_P - GF_1;
-      Two_P_Plus_1 : constant GF32 := Two_P + GF_1;
+      Two_P : constant Normal_GF := P * GF_2;
+      Two_P_Minus_1 : constant Normal_GF := Two_P - GF_1;
+      Two_P_Plus_1 : constant Normal_GF := Two_P + GF_1;
    begin
       A := (others => 0);
-      PDebug.DH ("Pack Stress - Case 1 - A is", A);
+      PDebug.DH16 ("Pack Stress - Case 1 - A is", A);
       R := Utils.Pack_25519 (A);
       Debug.DH ("Pack Stress - Case 1 - R is", R);
 
       A := (others => 65535);
-      PDebug.DH ("Pack Stress - Case 2 - A is", A);
+      PDebug.DH16 ("Pack Stress - Case 2 - A is", A);
       R := Utils.Pack_25519 (A);
       Debug.DH ("Pack Stress - Case 2 - R is", R);
 
       A := P;
-      PDebug.DH ("Pack Stress - Case 3 - A is P", A);
+      PDebug.DH16 ("Pack Stress - Case 3 - A is P", A);
       R := Utils.Pack_25519 (A);
       Debug.DH ("Pack Stress - Case 3 - R is", R);
 
       A := P_Minus_1;
-      PDebug.DH ("Pack Stress - Case 4 - A is P_Minus_1", A);
+      PDebug.DH16 ("Pack Stress - Case 4 - A is P_Minus_1", A);
       R := Utils.Pack_25519 (A);
       Debug.DH ("Pack Stress - Case 4 - R is", R);
 
       A := P_Plus_1;
-      PDebug.DH ("Pack Stress - Case 5 - A is P_Plus_1", A);
+      PDebug.DH16 ("Pack Stress - Case 5 - A is P_Plus_1", A);
       R := Utils.Pack_25519 (A);
       Debug.DH ("Pack Stress - Case 5 - R is", R);
 
       A := Two_P;
-      PDebug.DH ("Pack Stress - Case 6 - A is Two_P", A);
+      PDebug.DH16 ("Pack Stress - Case 6 - A is Two_P", A);
       R := Utils.Pack_25519 (A);
       Debug.DH ("Pack Stress - Case 6 - R is", R);
 
       A := Two_P_Minus_1;
-      PDebug.DH ("Pack Stress - Case 7 - A is Two_P_Minus_1", A);
+      PDebug.DH16 ("Pack Stress - Case 7 - A is Two_P_Minus_1", A);
       R := Utils.Pack_25519 (A);
       Debug.DH ("Pack Stress - Case 7 - R is", R);
 
       A := Two_P_Plus_1;
-      PDebug.DH ("Pack Stress - Case 8 - A is Two_P_Plus_1", A);
+      PDebug.DH16 ("Pack Stress - Case 8 - A is Two_P_Plus_1", A);
       R := Utils.Pack_25519 (A);
       Debug.DH ("Pack Stress - Case 8 - R is", R);
 
