dffd535587bfbc1d1dfe3f1e94c50de3406f8ebb
Rod Chapman <rod@proteancode.co>
2021-12-02 11:21:33 +0000

    Additionally test Open in both SPARK and TweetNaCl

--- perf/tsign.adb
+++ perf/tsign.adb
@@ -5,36 +5,51 @@ with FE310.CLINT;
 with FE310.Time; use FE310.Time;
 
 with Interfaces; use Interfaces;
+with Interfaces.C;
 
 with IO;
 
 with SPARKNaCl;      use SPARKNaCl;
 with SPARKNaCl.Sign; use SPARKNaCl.Sign;
-with SPARKNaCl.Sign.Utils; use SPARKNaCl.Sign.Utils;
+--  with SPARKNaCl.Sign.Utils; use SPARKNaCl.Sign.Utils;
 
 with TweetNaCl_API;
 
 with RISCV.CSR; use RISCV.CSR;
 with FE310.Performance_Monitor; use FE310.Performance_Monitor;
 
+use type Interfaces.C.int;
+
 procedure TSign is
    subtype U64 is Unsigned_64;
 
    M : constant Byte_Seq (0 .. 255) := (0 => 16#55#, others => 16#aa#);
-
    SK : Signing_SK;
+   PK : Signing_PK;
    SM1 : Byte_Seq (0 .. 319) := (others => 0);
    SM2 : Byte_Seq (0 .. 319) := (others => 0);
 
-   My_SK : constant Bytes_64 :=
+   M2 : Byte_Seq (0 .. 319);
+   M2Len : I32;
+   M3 : Byte_Seq (0 .. 319);
+   M3Len : I32;
+   Open_OK : Boolean;
+
+   Raw_SK : constant Bytes_32 :=
      (16#56#, 16#89#, 16#60#, 16#72#, 16#D7#, 16#E1#, 16#B5#, 16#35#,
       16#2B#, 16#12#, 16#B6#, 16#FC#, 16#69#, 16#94#, 16#F3#, 16#76#,
       16#A7#, 16#5C#, 16#42#, 16#DF#, 16#70#, 16#1E#, 16#AC#, 16#F0#,
-      16#A0#, 16#EF#, 16#30#, 16#C6#, 16#A1#, 16#D8#, 16#27#, 16#F6#,
-      16#D6#, 16#40#, 16#DA#, 16#F4#, 16#0B#, 16#0B#, 16#35#, 16#EF#,
-      16#03#, 16#25#, 16#5B#, 16#EF#, 16#A3#, 16#4E#, 16#31#, 16#D4#,
-      16#35#, 16#A1#, 16#A2#, 16#E2#, 16#FF#, 16#AA#, 16#EA#, 16#72#,
-      16#82#, 16#82#, 16#D2#, 16#D0#, 16#93#, 16#6C#, 16#19#, 16#10#);
+      16#A0#, 16#EF#, 16#30#, 16#C6#, 16#A1#, 16#D8#, 16#27#, 16#F6#);
+
+--   My_SK : constant Bytes_64 :=
+--     (16#56#, 16#89#, 16#60#, 16#72#, 16#D7#, 16#E1#, 16#B5#, 16#35#,
+--      16#2B#, 16#12#, 16#B6#, 16#FC#, 16#69#, 16#94#, 16#F3#, 16#76#,
+--      16#A7#, 16#5C#, 16#42#, 16#DF#, 16#70#, 16#1E#, 16#AC#, 16#F0#,
+--      16#A0#, 16#EF#, 16#30#, 16#C6#, 16#A1#, 16#D8#, 16#27#, 16#F6#,
+--      16#D6#, 16#40#, 16#DA#, 16#F4#, 16#0B#, 16#0B#, 16#35#, 16#EF#,
+--      16#03#, 16#25#, 16#5B#, 16#EF#, 16#A3#, 16#4E#, 16#31#, 16#D4#,
+--      16#35#, 16#A1#, 16#A2#, 16#E2#, 16#FF#, 16#AA#, 16#EA#, 16#72#,
+--      16#82#, 16#82#, 16#D2#, 16#D0#, 16#93#, 16#6C#, 16#19#, 16#10#);
 
    T1, T2 : UInt64;
 
@@ -49,6 +64,12 @@ procedure TSign is
                          M  : in     Byte_Seq;
                          SK : in     Signing_SK);
 
+   procedure Tweet_Open (M      :    out Byte_Seq;
+                         Status :    out Boolean;
+                         MLen   :    out I32;
+                         SM     : in     Byte_Seq;
+                         PK     : in     Signing_PK);
+
    procedure Tweet_Sign (SM :    out Byte_Seq;
                          M  : in     Byte_Seq;
                          SK : in     Signing_SK)
@@ -61,6 +82,21 @@ procedure TSign is
                                  M'Length,
                                  SK);
    end Tweet_Sign;
+
+   procedure Tweet_Open (M      :    out Byte_Seq;
+                         Status :    out Boolean;
+                         MLen   :    out I32;
+                         SM     : in     Byte_Seq;
+                         PK     : in     Signing_PK)
+   is
+      R : Interfaces.C.int;
+      LMLen : Unsigned_64;
+   begin
+      R := TweetNaCl_API.Crypto_Sign_Open (M, LMLen, SM, SM'Length, PK);
+      Status := (R = 0);
+      MLen := I32 (LMLen);
+   end Tweet_Open;
+
 
    procedure Report
    is
@@ -93,7 +129,8 @@ begin
    IO.Put_Line ("CPU Frequency reset was: ", U64 (CPU_Hz1));
    IO.Put_Line ("CPU Frequency now is:    ", U64 (CPU_Hz2));
 
-   Construct (My_SK, SK);
+   SPARKNaCl.Sign.Keypair (Raw_SK, PK, SK);
+--   Construct (My_SK, SK);
 
    FE310.Performance_Monitor.Set_Commit_Events (3, No_Commit_Events);
    FE310.Performance_Monitor.Set_Commit_Events (4, No_Commit_Events);
@@ -127,6 +164,14 @@ begin
    Total_Time := Unsigned_64 (T2 - T1);
    Report;
 
+   IO.Put_Line ("SPARKNaCl.Open test");
+   T1 := Mcycle.Read;
+   SPARKNaCl.Sign.Open (M2, Open_OK, M2Len, SM1, PK);
+   T2 := Mcycle.Read;
+   Total_Time := Unsigned_64 (T2 - T1);
+   Report;
+   IO.Put_Line (if Open_OK then "Open OK" else "Open FAIL");
+
    Turn_Off (Red_LED);
    Turn_On (Green_LED);
 
@@ -139,6 +184,16 @@ begin
    Total_Time := Unsigned_64 (T2 - T1);
 
    Report;
+
+   IO.Put_Line ("TweetNaCl.Open test");
+   T1 := Mcycle.Read;
+   Tweet_Open (M3, Open_OK, M3Len, SM2, PK);
+   T2 := Mcycle.Read;
+   Total_Time := Unsigned_64 (T2 - T1);
+   Report;
+   IO.Put_Line (if Open_OK then "Open OK" else "Open FAIL");
+
+
 
    Turn_Off (Green_LED);
 
