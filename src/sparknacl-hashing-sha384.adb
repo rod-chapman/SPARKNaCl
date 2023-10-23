83dea19b778de78426b4d5264d0cb5fced998af4
mxar <max.arthur@scheven-office.com>
2023-10-23 19:10:42 +0200

    Replace TS64 with Big_Endian_Unpack in SHA384 pkg
    This commit replaces the TS64 function with the Big_Endian_Unpack
    function. Both functions are functionally identical but differ in their
    implementation and what is proved about them.

--- src/sparknacl-hashing-sha384.adb
+++ src/sparknacl-hashing-sha384.adb
@@ -237,7 +237,7 @@ is
 
       for I in Index_8 loop
          pragma Loop_Optimize (No_Unroll);
-         X (8 * I .. (8 * I + 7)) := TS64 (Z (I));
+         X (8 * I .. (8 * I + 7)) := Big_Endian_Unpack (Z (I));
       end loop;
 
    end Hashblocks_384;
@@ -271,11 +271,11 @@ is
       ML_LSBs := U64 (M'Length) * 8;
       if B < 112 then
          X (119) := ML_MSB;
-         X (120 .. 127) := TS64 (ML_LSBs);
+         X (120 .. 127) := Big_Endian_Unpack (ML_LSBs);
          Hashblocks_384 (H, X (0 .. 127));
       else
          X (247) := ML_MSB;
-         X (248 .. 255) := TS64 (ML_LSBs);
+         X (248 .. 255) := Big_Endian_Unpack (ML_LSBs);
          Hashblocks_384 (H, X);
       end if;
 
